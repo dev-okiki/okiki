7797328c1eb4d0a07f1dcc83eb20db41412fa654
dev-okiki <127994239+dev-okiki@users.noreply.github.com>
2023-03-18 20:46:51 +0100

    Create okiki2correct.php

---
+++ okiki2correct.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+Validations to check
+<Option value="check"> Check if voter is above 18 years"</option>
+   
+<script>
+<Option value ="check"> check "if" "voter has PVC"> </option>
+<Option value ="check"> check  "if" "Voter's ward is 020"</option>
+echo "Voter eligible to vote<br>";
+echo "Why ",  "it",  "fails.";
+?> 
+</script>
+</body>
+</html>
