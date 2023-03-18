c0a1a34a3a80013ca851fe6ad944600678ee3732
dev-okiki <127994239+dev-okiki@users.noreply.github.com>
2023-03-18 20:12:31 +0100

    Create okiki2.php

---
+++ okiki2.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+Validations to check
+<Option value="check"> Check if voter is above 18 years"</option>
+   
+<script>
+<Option value ="check"> check "if" "voter has PVC"> </option>
+<Option value ="check"> check  "if" "Voter's ward is 020"</option>
+echo "<h2>PHP is Fun!</h2>";
+echo "Hello world!<br>";
+echo "Voter eligible to vote<br>";
+echo "Why ",  "it",  "fails.";
+?> 
+</script>
+</body>
+</html>
